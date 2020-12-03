a2ea1a6279abd9eece339434dec85f137f82b42f
Arijit Gupta <arijitgupta2000@gmail.com>
2020-12-03 16:46:08 +0530

    Updating latest functions

--- superres.py
+++ superres.py
@@ -18,6 +18,7 @@ import numpy as np
 import utils
 import os
 import logging
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import tensorflow as tf
 
@@ -49,9 +50,6 @@ model = "models/FSRCNN_x3.pb"
 modelName = model.split(os.path.sep)[-1].split("_")[0].split('/')[-1].lower()
 modelScale = model.split("_x")[-1]
 modelScale = int(modelScale[:modelScale.find(".")])
-
-# print("[INFO] Model name: {}".format(modelName))
-# print("[INFO] Model scale: {}".format(modelScale))
 sr = cv2.dnn_superres.DnnSuperResImpl_create()
 sr.readModel(model)
 sr.setModel(modelName, modelScale)
@@ -70,92 +68,57 @@ while True:
 	frame = cv2.flip(vs.read(),1)
 	# frame = vs.read()
 	sky = frame[0:250, 0:250]
-	gray = cv2.cvtColor(sky, cv2.COLOR_BGR2GRAY)
+	upscaled = imutils.resize(sr.upsample(sky), width = 250)
+	gray = cv2.cvtColor(upscaled, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (7, 7), 0)
-
-	# upscale the image using super-resolution
-	upscaled = imutils.resize(sr.upsample(sky), width = 125)
-	sky = imutils.resize(sky, width=400)
 
 	# draw rectangle over the roi where the hand should be
 	frame = cv2.rectangle(frame, (0,0), (250,250), (253,71,3), 3)
 
 	# calibrate the background for the threshold function for the first 30 frames
 	if num_frames < 30:
-		'''
-		frame = cv2.putText(frame, 'Hold Still, Calibrating....', (5,15), cv2.FONT_HERSHEY_DUPLEX,  
-                   0.5, (0,255,0), 1, cv2.LINE_AA) 
-		'''
 		utils.run_avg(gray, aWeight)
 	else:
 		if num_frames == 30:
-			'''
-			frame = cv2.putText(frame, 'Calibrated!', (5,15), cv2.FONT_HERSHEY_DUPLEX,  
-                   0.5, (0,255,0), 1, cv2.LINE_AA) 
-			'''	
 			print("[INFO] Calibrated! Please keep your hand inside the colored boundary for it to be detected.")
 
 		elif num_frames == 40:
-			'''
-			frame = cv2.putText(frame, 'You can recalibrate by pressing R', (5,15), cv2.FONT_HERSHEY_DUPLEX,  
-                   0.4, (0,255,0), 1, cv2.LINE_AA) 
-			'''
 			print("[INFO] You can press R to recalibrate")
+
 		hand = utils.segment(gray)
 		if hand is not None:
 			(thresholded, segmented) = hand
 
 			cv2.drawContours(frame, [segmented], -1, (0, 255, 0))
-			frame = imutils.resize(frame, height=125)
-			
+		
 			# display the segmented image
 			thresholded = imutils.resize(thresholded, height=125)
 			thresholded = np.stack((thresholded,)*3, axis=-1)
 			tfimage=cv2.flip(thresholded,1).reshape(-1,125,125,3)
-			'''
-			for i in range(125):
-				for j in range(125):
-					for k in range(3):
-						thresholded[i,j,k] = (thresholded[i,j,k]/255)*(gray[i,j]+20)
-			'''
 
-			#print(thresholded)
 			try:
 				temp = pred
 			except:
 				temp = (np.argmax(tfmodel.predict(tfimage), axis=-1)[0])
-			
 			pred = (np.argmax(tfmodel.predict(tfimage), axis=-1)[0])
+			message = "No Recognizable Command"
+			scale = 0.5
+			
 			if pred < num_keys:
 				if temp < num_keys:
 					keyboard.release(keys[temp]) and temp!=pred 
 				keyboard.press(keys[pred])
-
-			'''
-			if pred==1:
-				keyboard.release(key2)
-				keyboard.press(key1)
-				print(classes[pred])
-			
-			elif pred==2:
-				keyboard.release(key1)
-				keyboard.press(key2)
-				print(classes[pred])
-			'''
-			frame = np.concatenate((frame, thresholded), axis=1)
-		
-			# cv2.imshow("Input Recieved", thresholded)
-			# cv2.moveWindow('Input Recieved',340,0)
+				scale = 1
+				message = classes[pred]
+			upscaled = cv2.putText(upscaled, message, (4,24), cv2.FONT_HERSHEY_DUPLEX,  
+                   scale, (208, 218, 3), 1, cv2.LINE_AA) 
 
 	# display actual camera stream
-	frame = imutils.resize(frame, height=125)
+	frame = imutils.resize(frame, height=250)
 	frame = np.concatenate((frame, upscaled), axis=1)
 	
-	cv2.imshow("Live Feed", frame)
+	cv2.imshow("Live Feed", imutils.resize(frame, height=250))
 	cv2.moveWindow('Live Feed',0,0)
-
-	# dispaly upscaled cropped section
-	# cv2.imshow("Cropped Super Resolution", upscaled)
 
 	num_frames += 1
 
